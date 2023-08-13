6d029f37b42e6d07bd990c95620712e766ffa604
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 02:03:22 -0300

    config listtask design woth empty tasks

--- src/components/ListTask.tsx
+++ src/components/ListTask.tsx
@@ -1,9 +1,48 @@
+import { EmptyTask } from './EmptyTasks'
 import styles from './ListTask.module.css'
 
+export interface Task{
+    id:string
+    content: string,
+    isComplete: boolean
+}
+
+const tasks:Task[] = []
+
 export function ListTask(){
+
+    const hasTask = tasks.length > 0
+
+    function generateTasks(){
+        if(hasTask){
+            return tasks.map(task => {
+                return <div key={task.id}>Task</div>
+            })
+        }
+        return <EmptyTask />
+    }
+
     return (
-        <div className={styles.listTask}>
-            List Task
+        <div className={styles.wrapper}>
+            <div className={styles.listTask}>
+                <header>
+                    <div className={styles.createdTasks}>
+                        <p>Tarefas criadas</p>
+                        <div className={styles.counter}>
+                            <p>0</p>
+                        </div>
+                    </div>
+                    <div className={styles.completedTasks}>
+                        <p>Concluídas</p>
+                        <div className={styles.counter}>
+                            <p>0</p>
+                        </div>
+                    </div>
+                </header>
+                <main>
+                    {generateTasks()}
+                </main>
+            </div>
         </div>
     )
 }
