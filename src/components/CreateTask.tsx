8caf2bc1923505cca6b346aa0b5bb11f68932c09
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-12 23:31:14 -0300

    create component create task

---
+++ src/components/CreateTask.tsx
@@ -0,0 +1,9 @@
+import styles from './CreateTask.module.css'
+
+export function CreateTask(){
+    return (
+        <div className={styles.createTask}>
+            Create Task
+        </div>
+    )
+}
