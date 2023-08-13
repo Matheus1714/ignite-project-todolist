279211dfa130413f78fe5db52d33161038448569
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-12 22:46:35 -0300

    create mock

---
+++ src/database/data.tsx
@@ -0,0 +1,18 @@
+interface Task{
+    id:string
+    content: string,
+    isComplete: boolean
+}
+
+export const data:Task[] = [
+    {
+        id: 'adasd',
+        content: 'Terminar o desafio',
+        isComplete: true
+    },
+    {
+        id: 'adasd',
+        content: 'Estudar Typescript',
+        isComplete: false
+    },
+]
