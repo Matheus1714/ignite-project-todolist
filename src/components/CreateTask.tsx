5ff3a4b427726036c6fbf50cad8c4859e58c4099
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 14:24:19 -0300

    create interfaces to create task

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,11 +1,26 @@
 import { PlusCircle } from '@phosphor-icons/react'
 import styles from './CreateTask.module.css'
+import { ChangeEvent, FormEvent } from 'react'
 
-export function CreateTask(){
+interface CreateTaskProps{
+    newTaskContent: string
+    handleNewTaskChange: (event: ChangeEvent<HTMLInputElement>) => void
+    handleCreateNewTask: (event: FormEvent) => void
+}
+
+export function CreateTask({ 
+    newTaskContent, 
+    handleNewTaskChange, 
+    handleCreateNewTask }:CreateTaskProps){
     return (
         <div className={styles.wrapper}>
-            <form className={styles.createTask}>
-                <input type='text' placeholder='Adicione uma nova tarefa' />
+            <form onSubmit={handleCreateNewTask} className={styles.createTask}>
+                <input 
+                    type='text' 
+                    placeholder='Adicione uma nova tarefa'
+                    onChange={handleNewTaskChange}
+                    value={newTaskContent}
+                />
                 <button type='submit'>Criar <PlusCircle size={16}/></button>
             </form>
         </div>
