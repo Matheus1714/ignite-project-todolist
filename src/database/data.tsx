f9443c646bbe268fd29b4ab63f01acbc9a1f9de3
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 14:24:59 -0300

    add data example

--- src/database/data.tsx
+++ src/database/data.tsx
@@ -1,14 +1,31 @@
 import { Task } from "../components/ListTask"
+import { v4 as uuidv4 } from 'uuid'
 
 export const data:Task[] = [
     {
-        id: 'adasd',
-        content: 'Terminar o desafio',
+        id: uuidv4(),
+        content: 'Integer urna interdum ',
+        isComplete: false
+    },
+    {
+        id: uuidv4(),
+        content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+        isComplete: false
+    },
+    {
+        id: uuidv4(),
+        content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+        isComplete: false
+    },
+    {
+        id: uuidv4(),
+        content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
         isComplete: true
     },
     {
-        id: 'adasd',
-        content: 'Estudar Typescript',
-        isComplete: false
+        id: uuidv4(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum nihil et hic illo adipisci assumenda magnam qui illum vero, nam quae reprehenderit laudantium mollitia error deserunt quidem rerum officiis?',
+        isComplete: true
     },
 ]
+
