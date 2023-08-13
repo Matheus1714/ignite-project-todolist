4391cb3d993a0d15c7f76fdb39d3602f2dd24fc3
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 00:29:52 -0300

    create design create task component

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,9 +1,11 @@
+import { PlusCircle } from '@phosphor-icons/react'
 import styles from './CreateTask.module.css'
 
 export function CreateTask(){
     return (
-        <div className={styles.createTask}>
-            Create Task
-        </div>
+        <form className={styles.createTask}>
+            <input type='text' placeholder='Adicione uma nova tarefa' />
+            <button type='submit'>Criar <PlusCircle size={16}/></button>
+        </form>
     )
 }
