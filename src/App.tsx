3d51ebedea88d2fd1e187daf87a68f4883259a32
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-12 23:30:54 -0300

    update App with principal components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './global.css'
+import { CreateTask } from './components/CreateTask'
+import { Header } from './components/Header'
+import { ListTask } from './components/ListTask'
 
 export function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <main>
+      <Header />
+      <CreateTask />
+      <ListTask />
+    </main>
   )
 }
