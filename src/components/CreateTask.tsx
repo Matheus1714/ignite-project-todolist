6419e17cc9cdc1ef73a6a4f57c6d43044908fd8d
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 02:02:35 -0300

    adjust create task design with wrapper

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -3,9 +3,11 @@ import styles from './CreateTask.module.css'
 
 export function CreateTask(){
     return (
-        <form className={styles.createTask}>
-            <input type='text' placeholder='Adicione uma nova tarefa' />
-            <button type='submit'>Criar <PlusCircle size={16}/></button>
-        </form>
+        <div className={styles.wrapper}>
+            <form className={styles.createTask}>
+                <input type='text' placeholder='Adicione uma nova tarefa' />
+                <button type='submit'>Criar <PlusCircle size={16}/></button>
+            </form>
+        </div>
     )
 }
